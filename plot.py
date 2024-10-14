74e466566b02b34e1707b70f1c42a03f5c9eba1d
Zice37 <147526125+Zice37@users.noreply.github.com>
2024-10-14 16:44:38 +0200

    Added GUI (updated plot.py)

--- plot.py
+++ plot.py
@@ -1,58 +1,86 @@
-#!/usr/bin/python
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import shutil
+import hashlib
 
 
 #CSV v3 de RaceChrono
-
+LINEAS_A_ELIMINAR = [0,1,2,3,4,5,6,7,8,10,11]  
+    
 RENAME_PREFIX = '.copy-'
+RENAME_DIR = './.tmp'
 COLORS = ['blue', 'red', 'green', 'yellow', 'purple']
 
-def rename_file(filepath):
-    directory, filename = os.path.split(filepath)    
-    new_filename = RENAME_PREFIX + filename
-    return os.path.join(directory, new_filename)
+def clear_cache_directory():
+    # Verificar si el directorio existe antes de intentar eliminarlo
+    if os.path.exists(RENAME_DIR):
+        # Eliminar todo el contenido del directorio /cache
+        shutil.rmtree(RENAME_DIR)
+        # Volver a crear el directorio vacío
+        os.makedirs(RENAME_DIR)
 
-def eliminar_cabeceras(filename):
+def rename_file(filepath):
+    directory, filename = os.path.split(filepath)
+    hash_object = hashlib.sha256()
+    hash_object.update(filename.encode('utf-8'))
+    
+    new_filename = RENAME_PREFIX + hash_object.hexdigest()
+    return os.path.join(RENAME_DIR, new_filename)
+
+def preparar_csv(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
-    lineas_a_eliminar = [0,1,2,3,4,5,6,7,8,10,11]  
-    lines = [line for i, line in enumerate(lines) if i not in lineas_a_eliminar]
+    lines = [line for i, line in enumerate(lines) if i not in LINEAS_A_ELIMINAR]
     filename = rename_file(filename)
     with open(filename, 'w') as file:
         file.writelines(lines)
     return filename
 
-def read_csv(filename):
+def analizar_csv(filename):
     try:
         raw_data = pd.read_csv(filename)
     except:
         try:
-            raw_data = pd.read_csv(rename_file(filename))
+            file = rename_file(filename)
+            raw_data = pd.read_csv(file)
         except:
-            filename = eliminar_cabeceras(filename)
-            raw_data = pd.read_csv(filename)
+            try:
+                file = preparar_csv(filename)
+                raw_data = pd.read_csv(file)
+            except:
+                return None, None
 
+    try:
+        return file, set(raw_data['lap_number'])
+    except:
+        return None, None
+
+def trim_csv(filename, lap):
+    #filename ya esta preparado
+    raw_data = pd.read_csv(filename)
+    
     data = {
         'distance_traveled': [], #raw_data['distance_traveled'],
         'speed': [], #raw_data['speed'],
     }
-
-    initial_lap = raw_data['lap_number'][0]
+    
+    start_index = 0
     for i in range(len(raw_data['lap_number'])):
-        if raw_data['lap_number'][i] == ((initial_lap or 0) +1):
+        if raw_data['lap_number'][i] == lap:
             start_index = i
             break
             
+    
+    stop_index = len(raw_data['lap_number'])-1
     for i in range(len(raw_data['lap_number'])-1 , -1, -1):
-        if raw_data['lap_number'][i] == ((initial_lap or 0) +1):
+        if raw_data['lap_number'][i] == lap:
             stop_index = i
             break
 
-    data['distance_traveled'] = raw_data['distance_traveled'][start_index:stop_index].values
-    data['speed'] = raw_data['speed'][start_index:stop_index].values
+    data['distance_traveled'] = normalize_distance(raw_data['distance_traveled'][start_index:stop_index].values)
+    data['speed'] = ms_to_kph(raw_data['speed'][start_index:stop_index].values)
     return data
 
 def ms_to_kph(lista):
@@ -65,21 +93,25 @@ def normalize_distance(lista):
     return [x-offset for x in lista]
 
 def main(filenames):
+    if not os.path.exists(RENAME_DIR):
+        os.makedirs(RENAME_DIR)
+
+
     plt.figure(figsize=(100, 60))
-    i = 0
+    for i in range(len(filenames)):
+        current_file, laps = analizar_csv(filenames[i])
+        if current_file is None or laps is None:
+            print("El fichero "+ filenames[i]+" no cumple la especificación requerida")
+            continue
 
-    while i < len(filenames):
+        print("File: "+filenames[i]+" tiene las siguientes vueltas")
+        for lap in laps:
+            print(lap)
+        lap = int(input("Introduce cual ver: "))
+        assert(lap in laps)
+        data = trim_csv(current_file, lap)
 
-        data = read_csv(filenames[i])
-        distancia = data['distance_traveled']
-        distancia = normalize_distance(distancia)  
-
-        velocidad = data['speed']
-        velocidad = ms_to_kph(velocidad)
-
-
-        plt.plot(distancia, velocidad, label=filenames[i][:-4], color=COLORS[i%len(COLORS)])
-        i += 1
+        plt.plot(data['distance_traveled'], data['speed'], label=filenames[i][:-4], color=COLORS[i%len(COLORS)])
         
     plt.title('Laptimes')
     plt.xlabel('Distancia recorrida (metros)')
@@ -87,10 +119,12 @@ def main(filenames):
     plt.legend()
     plt.grid(True)
 
+    clear_cache_directory()
+
     plt.show()
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print(f"Use: python {sys.argv[0]} racechrono_csv_v3_hotlap1.csv racechrono_csv_v3_hotlap2.csv ... racechrono_csv_v3_hotlapN.csv")
+    if len(sys.argv) < 2:
+        print(f"Use: python {sys.argv[0]} racechrono_csv_v3_hotlap1.csv ... racechrono_csv_v3_hotlapN.csv")
     else:
         main(sys.argv[1:])
